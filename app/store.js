15423251882bed82a35c78e8c1fa087acb13d8ae
John Schoeman <johnschoeman1617@gmail.com>
2018-11-13 07:56:22 -0600

    Add redux-devtools-extension

--- app/store.js
+++ app/store.js
@@ -1,5 +1,6 @@
 import { createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 import rootReducer from './reducers'
 
-export default createStore(rootReducer)
+export default createStore(rootReducer, composeWithDevTools())
