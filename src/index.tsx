7d984e11b9f42842c39e91e3b72f7e47d126f30b
Devin Jameson <devinjameson@me.com>
2021-01-13 17:31:16 -0500

    Add example typographic styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,47 @@
 import React, { FunctionComponent } from "react"
-import { ScrollView, SafeAreaView, StyleSheet, View } from "react-native"
+import { ScrollView, StyleSheet, View } from "react-native"
+import StaticSafeAreaInsets from "react-native-static-safe-area-insets"
 
 import BaseExample from "./BaseExample"
 import ColorExample from "./ColorExample"
+import TypographyExample from "./TypographyExample"
 
 import { Sizing, Outlines, Colors } from "./styles"
 
 const App: FunctionComponent = () => {
   return (
-    <SafeAreaView>
-      <ScrollView contentContainerStyle={style.contentContainer}>
-        <View style={style.sectionContainer}>
-          <BaseExample />
-        </View>
-        <View style={style.sectionContainer}>
-          <ColorExample />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <ScrollView
+      style={style.container}
+      contentContainerStyle={style.contentContainer}
+    >
+      <View style={style.sectionContainer}>
+        <BaseExample />
+      </View>
+      <View style={style.sectionContainer}>
+        <ColorExample />
+      </View>
+      <View style={style.sectionContainer}>
+        <TypographyExample />
+      </View>
+    </ScrollView>
   )
 }
 
+const topInset = StaticSafeAreaInsets.safeAreaInsetsTop
+
 const style = StyleSheet.create({
+  container: {
+    marginTop: topInset,
+    backgroundColor: Colors.neutral.white,
+  },
   contentContainer: {
     padding: Sizing.x20,
   },
   sectionContainer: {
-    borderBottomWidth: Outlines.borderWidth.thin,
-    borderColor: Colors.neutral.s100,
     paddingBottom: Sizing.x20,
     marginBottom: Sizing.x20,
+    borderBottomWidth: Outlines.borderWidth.thin,
+    borderColor: Colors.neutral.s100,
   },
 })
 
